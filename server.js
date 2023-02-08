333d2df7de3bd3c0f98a2073fe767723f1287686
Berheruben <rubenberhe56@yourdomain.com>
2023-02-08 18:33:30 +0100

    cleaned the file

--- server.js
+++ server.js
@@ -1,53 +1,59 @@
-const express = require('express');
-const app = express();
-const multer = require("multer");
-const path = require("path");
-const fs = require('fs');
-// storage engine 
+import express from 'express'
+import multer from 'multer'
+import fileService from './services/file.js'
+
+const PORT = 8080
+export const app = express()
 
 const storage = multer.diskStorage({
-    destination: './uploads/',
-    filename: (req, file, cb) => {
-        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
-    }
+  destination: './uploads/',
+  filename: (_, file, cb) => {
+    return cb(null, `${file.originalname}`)
+  }
 })
-
-const upload = multer({
-    storage: storage
-    
-})
+const upload = multer({ storage })
 // Endpoint to upload an image
 app.post('/images', upload.single('image'), (req, res) => {
-    const file = req.file;
-    res.status(201).send({ fileName: file.filename });
-  });
-  
-  // Endpoint to list all uploaded images by name
-app.get('/images', (req, res) => {
-    fs.readdir('uploads/', (err, files) => {
-      if (err) {
-        return res.status(500).send({ message: 'Failed to read uploaded files' });
-      }
-  
-      res.status(200).send({
-        files: files.map(fileName => ({ fileName }))
-      });
-    });
-  });
-  // Endpoint to delete an uploaded image
-  app.delete('/images/:fileName', (req, res) => {
-    const fileName = req.params.fileName;
-  
-    fs.unlink(`uploads/${fileName}`, (err) => {
-      if (err) {
-        return res.status(500).send({ message: 'Failed to delete the file' });
-      }
-  
-      res.status(200).send({ message: 'File deleted successfully' });
-    });
-  });  
+  const file = req.file
 
+  return res.status(201).send({ fileName: file.filename })
+})
 
-app.listen(3000, () => {
-    console.log('Server started on http://localhost:3000');
-  });
+// Endpoint to list all uploaded images by name
+app.get('/images', async (_, res) => {
+  try {
+    const files = await fileService.getFiles()
+    return res.status(200).send({ images: files.map(file => ({ name: file })) })
+  } catch (error) {
+    return res.status(500).send({ message: 'Failed to read uploaded files' })
+  }
+})
+
+// Endpoint to resize an uploaded image
+app.get('/images/:name/resize', async (req, res) => {
+  const width = parseInt(req.query.width)
+  const height = parseInt(req.query.height)
+  const name = req.params.name
+  try {
+    const fileBuffer = await fileService.resizeFile(name, width, height)
+    return res.status(200).contentType('image/jpeg').send(fileBuffer)
+  } catch (error) {
+    // TODO: map errors end status codes
+    res.status(500).send({ error: error.message })
+  }
+})
+
+// Endpoint to delete an uploaded image
+app.delete('/images/:name', async (req, res) => {
+  const name = req.params.name
+  try {
+    await fileService.deleteFile(name)
+    return res.status(200).send({ message: 'File deleted successfully' })
+  } catch (error) {
+    return res.status(500).send({ message: 'Failed to delete the file' })
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`)
+})
